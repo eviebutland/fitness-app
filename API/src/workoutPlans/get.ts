22860b85952ed1a21f65b499b2b3f77ace1560ca
EvieButland <evie.butland@hectare.farm>
2023-03-02 13:14:18 +0000

    adding file back

---
+++ API/src/workoutPlans/get.ts
@@ -0,0 +1,156 @@
+import { Context } from 'openapi-backend'
+import { client } from '../../server'
+import { rollback } from '../utils/rollback'
+import { Request, Response } from 'express'
+import { QueryResult } from 'pg'
+import { User } from '../lib/types/user'
+
+export const getAllWorkoutPlans = async (api: Context, request: Request, response: Response): Promise<void> => {
+  try {
+    await client.query('BEGIN TRANSACTION')
+    const query = 'SELECT * FROM workoutPlans'
+    const results = await client.query(query)
+
+    await client.query('COMMIT TRANSACTION')
+    response.status(200).json({ data: results.rows })
+  } catch (error) {
+    rollback(client)
+    console.log(error)
+    response.status(500).json({ message: 'Something went wrong', error })
+  }
+}
+
+export const getWorkoutPlanById = async (api: Context, request: Request, response: Response): Promise<void> => {
+  if (api.request.params.id === ':id') {
+    response.status(400).json({ message: 'Please provide an ID' })
+    return
+  }
+
+  try {
+    await client.query('BEGIN TRANSACTION')
+    const query = `
+      SELECT * FROM workoutPlans
+      WHERE id = $1`
+
+    const results: QueryResult = await client.query(query, [api.request.params.id])
+
+    const formattedResult = results.rows.filter(value => value !== null)
+
+    await client.query('COMMIT TRANSACTION')
+    response.status(200).json({ data: formattedResult })
+  } catch (error) {
+    rollback(client)
+    console.log(error)
+    response.status(500).json({ message: 'Something went wrong', error })
+  }
+}
+
+const handleSelectAllExercisesInCategory = async (category: string, response: Response) => {
+  const query = `
+      SELECT * FROM workoutPlans
+      WHERE title = '${category}'`
+  try {
+    const results = await client.query(query)
+
+    if (results.rows.length) {
+      const formattedResult = results.rows.filter(value => value !== null)
+
+      const response = { data: formattedResult, total: formattedResult.length }
+      return response
+    } else {
+      return { data: 'REST', total: 0 }
+    }
+  } catch (error) {
+    rollback(client)
+    console.log(error)
+    response.status(500).json({ message: 'Something went wrong', error })
+  }
+}
+
+export const getTodaysWorkout = async (api: Context, request: Request, response: Response) => {
+  // Based off the user's logged in workout preference find a workout that matches
+  const user: User = api.request?.user
+  const query = api.request.query.day as string
+  const userWorkoutPreference =
+    typeof user.workoutpreference === 'string' ? JSON.parse(user.workoutpreference) : user?.workoutpreference
+
+  const todaysWorkoutCatergory = userWorkoutPreference[query]
+
+  const workout = await handleSelectAllExercisesInCategory(todaysWorkoutCatergory.toLowerCase(), response)
+
+  // // const courier: ICourierClient = CourierClient({ authorizationToken: 'pk_prod_MJAHFWSKV24TJXQJAV7KHKC975SW' })
+
+  let selectedWorkout: Record<string, string> = {}
+
+  const userCompletedWorkouts =
+    typeof user.completedworkouts === 'string' ? JSON.parse(user.completedworkouts) : user.completedworkouts
+
+  if (workout?.data.length && typeof workout?.data !== 'string') {
+    // check if any of the workouts have been completed yet
+    userCompletedWorkouts.forEach((completedWorkout: number) => {
+      if (workout.data.length && typeof workout.data !== 'string') {
+        selectedWorkout = workout.data.find(workout => completedWorkout !== Number(workout.id)) ?? {}
+      } else {
+        selectedWorkout = {}
+      }
+    })
+  }
+  try {
+    const emailData = workout?.data[0]
+
+    // await courier.send({
+    //   message: {
+    //     to: {
+    //       email: 'evie.butland@gmail.com'
+    //     },
+    //     template: 'HBDVP38QPSMS4YG676E20DGYP7X6',
+    //     data: {
+    //       recipientName: 'Evie',
+    //       workoutName: emailData.name,
+    //     }
+    //   }
+    // })
+
+    if (selectedWorkout?.id) {
+      const updatedCompletedWorkouts = [...userCompletedWorkouts, Number(selectedWorkout?.id)]
+
+      await client.query(
+        `UPDATE users
+        SET completedworkouts = $1
+        WHERE id = ${api.request.user.id}
+        `,
+        [JSON.stringify(updatedCompletedWorkouts)]
+      )
+      await client.query('COMMIT TRANSACTION')
+      // Update user to have completed the workout
+    }
+    response.status(200).json({ message: 'Succesfully sent', workout })
+  } catch (error) {
+    console.log(error)
+    rollback(client)
+    response.status(500).json({ message: 'Something went wrong', error })
+  }
+}
+
+export const getAllExercisesInCatergory = async (
+  api: Context,
+  request: Request,
+  response: Response
+): Promise<void> => {
+  try {
+    if (typeof api.request.params.catergory === 'string' && api.request.params.catergory) {
+      await client.query('BEGIN TRANSACTION')
+      const results = await handleSelectAllExercisesInCategory(api.request.params.catergory, response)
+      response.status(200).json(results)
+    } else {
+      response.status(404).json({ message: 'Please provide a catergory to search by' })
+      return
+    }
+
+    await client.query('COMMIT TRANSACTION')
+  } catch (error) {
+    rollback(client)
+    console.log(error)
+    response.status(500).json({ message: 'Something went wrong', error })
+  }
+}
