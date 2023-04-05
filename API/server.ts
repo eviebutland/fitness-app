bc172c5f22a06f40ebef73b99b722e81c3a778d1
EvieButland <evie.butland@hectare.farm>
2023-04-05 07:40:50 +0100

    connecting to RDS instance

--- API/server.ts
+++ API/server.ts
@@ -5,11 +5,11 @@ dotenv.config()
 
 // TODO store these values in AWS
 export const client: Client = new Client({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: 5432
+  user: process.env.RDS_USERNAME,
+  host: process.env.RDS_HOSTNAME,
+  database: process.env.RDS_DB_NAME,
+  password: process.env.RDS_PASSWORD,
+  port: parseInt(process.env.RDS_PORT || '5432')
 })
 
 export const connectDb = async (): Promise<void> => {
